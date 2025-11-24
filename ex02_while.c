a8d3d6fd79a5c3663b84ee44c85e3aeb1de87399
danilobenatti-create <danilo.benatti@escola.pr.gov.br>
2025-11-24 08:29:04 -0300

    Update ex02_while.c

--- ex02_while.c
+++ ex02_while.c
@@ -1,28 +1,16 @@
-ALGORITMO SomaMediaSentinela
-INÍCIO
-  VAR num, soma, quantidade: INTEIRO
-  VAR media: REAL
+ #include <stdio.h>
 
-  soma = 0
-  quantidade = 0
+int main() {
+    int N;   
+    int i;
+        printf("Digite um número inteiro (N) para ver sua tabuada: ");
+    scanf("%d", &N);
 
-  ESCREVA "Digite números inteiros (digite -1 para parar):"
-  LEIA num
+   
+    printf("\n--- Tabuada do %d ---\n", N);
 
-  ENQUANTO num != -1 FAÇA
-    soma = soma + num
-    quantidade = quantidade + 1
-    LEIA num
-  FIM_ENQUANTO
+       for (i = 1; i <= 10; i++) {
+                printf("%d x %d = %d\n", N, i, N * i);
+    }
 
-  ESCREVA "Quantidade de números digitados: ", quantidade
-  ESCREVA "Soma total: ", soma
-
-  // Evita divisão por zero se o usuário digitar -1 primeiro
-  SE quantidade > 0 ENTÃO
-    media = (REAL)soma / quantidade
-    ESCREVA "Média: ", media
-  SENÃO
-    ESCREVA "Nenhum número foi inserido."
-  FIM_SE
-FIM
+    return 0;}
