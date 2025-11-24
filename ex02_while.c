1c361091a56734cbedda636dc5c1540455676ba0
danilobenatti-create <danilo.benatti@escola.pr.gov.br>
2025-11-24 08:29:39 -0300

    Update ex02_while.c

--- ex02_while.c
+++ ex02_while.c
@@ -1,16 +1,43 @@
- #include <stdio.h>
+  #include <stdio.h>
 
 int main() {
-    int N;   
-    int i;
-        printf("Digite um número inteiro (N) para ver sua tabuada: ");
-    scanf("%d", &N);
+    int numero;
+    int soma = 0;        
+    int quantidade = 0;    
+    double media = 0.0;  
+    printf("--- Calculadora de Soma e Média ---\n");
+    printf("Digite inteiros sucessivos.\n");
+    printf("Digite -1 para PARAR e ver os resultados.\n\n");
 
-   
-    printf("\n--- Tabuada do %d ---\n", N);
-
-       for (i = 1; i <= 10; i++) {
-                printf("%d x %d = %d\n", N, i, N * i);
+       printf("Digite um número: ");
+    if (scanf("%d", &numero) != 1) {
+        printf("Erro na leitura inicial.\n");
+        return 1;
     }
 
-    return 0;}
+    while (numero != -1) {
+              soma = soma + numero;
+        quantidade = quantidade + 1;
+
+              printf("Digite outro número: ");
+        if (scanf("%d", &numero) != 1) {
+             printf("Erro na leitura. Interrompendo a leitura.\n");
+             break; // Sai do loop em caso de erro
+        }
+    }
+
+   )
+    printf("\n--- Resultados ---\n");
+    
+       printf("Quantidade de números lidos (desconsiderando o -1): %d\n", quantidade);
+    printf("Soma total dos números: %d\n", soma);
+
+      if (quantidade > 0) {
+        media = (double)soma / quantidade;
+        printf("Média dos números: %.2lf\n", media); // Formato .2lf para duas casas decimais
+    } else {
+        printf("Nenhum número válido foi digitado antes de -1.\n");
+    }
+
+    return 0;
+}
