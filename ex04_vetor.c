76858545c83b0e261f056dc397189db812dc9501
danilobenatti-create <danilo.benatti@escola.pr.gov.br>
2025-11-24 08:31:21 -0300

    Update ex04_vetor.c

--- ex04_vetor.c
+++ ex04_vetor.c
@@ -1,39 +1,16 @@
-ALGORITMO AnaliseVetor
-INÍCIO
-  VAR vetor[10]: INTEIRO
-  VAR i, maior, menor: INTEIRO
-  VAR soma: INTEIRO
-  VAR media: REAL
+ #include <stdio.h>
 
-  soma = 0
+int main() {
+    int N; 
+    int i;
+      printf("Digite um número inteiro (N): ");
+    scanf("%d", &N);
 
-  // Leitura do vetor
-  PARA i DE 0 ATÉ 9 FAÇA
-    ESCREVA "Digite o valor ", i + 1, ":"
-    LEIA vetor[i]
-  FIM_PARA
+       printf("\n--- Tabuada do %d ---\n", N);
 
-  // Inicializa maior e menor com o primeiro elemento
-  maior = vetor[0]
-  menor = vetor[0]
-  soma = vetor[0] // Inicia a soma
+       for (i = 1; i <= 10; i++) {
+                printf("%d x %d = %d\n", N, i, N * i);
+    }
 
-  // Processamento (começa do segundo elemento, índice 1)
-  PARA i DE 1 ATÉ 9 FAÇA
-    soma = soma + vetor[i]
-
-    SE vetor[i] > maior ENTÃO
-      maior = vetor[i]
-    FIM_SE
-
-    SE vetor[i] < menor ENTÃO
-      menor = vetor[i]
-    FIM_SE
-  FIM_PARA
-
-  media = (REAL)soma / 10
-
-  ESCREVA "Maior valor: ", maior
-  ESCREVA "Menor valor: ", menor
-  ESCREVA "Média: ", media
-FIM
+    return 0;
+}
