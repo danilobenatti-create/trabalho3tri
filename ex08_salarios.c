4858e0d0aab6079f1c65863fc057324241613945
danilobenatti-create <danilo.benatti@escola.pr.gov.br>
2025-11-24 08:33:14 -0300

    Update ex08_salarios.c

--- ex08_salarios.c
+++ ex08_salarios.c
@@ -1,48 +1,54 @@
-ALGORITMO MenuSalarios
-INÍCIO
-  VAR N, i, opcao: INTEIRO
-  VAR salarios[10]: REAL
-  VAR soma, media: REAL
+#include <stdio.h>
 
-  ESCREVA "Quantos salários deseja cadastrar (1 a 10)?"
-  LEIA N
-  // (Opcional: Adicionar validação para N estar entre 1 e 10)
 
-  // Leitura dos salários
-  PARA i DE 0 ATÉ N - 1 FAÇA
-    ESCREVA "Digite o salário ", i + 1, ":"
-    LEIA salarios[i]
-  FIM_PARA
+#define MAX_N 10
 
-  // Menu de opções
-  FAÇA
-    ESCREVA "--- MENU SALÁRIOS ---"
-    ESCREVA "1) Listar salários"
-    ESCREVA "2) Média dos salários"
-    ESCREVA "0) Sair"
-    ESCREVA "Escolha uma opção:"
-    LEIA opcao
 
-    SE opcao == 1 ENTÃO
-      ESCREVA "Lista de Salários:"
-      PARA i DE 0 ATÉ N - 1 FAÇA
-        ESCREVA "Salário ", i + 1, ": R$ ", salarios[i]
-      FIM_PARA
-      
-    SENÃO SE opcao == 2 ENTÃO
-      soma = 0
-      PARA i DE 0 ATÉ N - 1 FAÇA
-        soma = soma + salarios[i]
-      FIM_PARA
-      media = soma / N
-      ESCREVA "Média dos salários: R$ ", media
-      
-    SENÃO SE opcao == 0 ENTÃO
-      ESCREVA "Saindo..."
-    SENÃO
-      ESCREVA "Opção inválida."
-    FIM_SE
-    ESCREVA "---------------------"
+void listar_salarios(float salarios[], int N);
+void calcular_media(float salarios[], int N);
+
+int main() {
+    float salarios[MAX_N]; // Vetor para armazenar os salários
+    int N;                
+    int opcao;            
+       do {
+        printf("Informe o número de salários (N) a serem lidos (1 a %d): ", MAX_N);
+        if (scanf("%d", &N) != 1) {
+           
+            while (getchar() != '\n');
+            N = -1; // Garante que o loop continue
+        }
+        if (N < 1 || N > MAX_N) {
+            printf("Erro: N deve ser um número entre 1 e %d.\n", MAX_N);
+        }
+    } while (N < 1 || N > MAX_N);
+
+        printf("\n--- Leitura dos Salários ---\n");
+    for (int i = 0; i < N; i++) {
+        do {
+            printf("Salário do colaborador %d: R$ ", i + 1);
+            if (scanf("%f", &salarios[i]) != 1 || salarios[i] < 0) {
+                printf("Erro: Salário inválido. Digite um valor positivo.\n");
+                // Limpa o buffer para nova tentativa
+                while (getchar() != '\n');
+            } else {
+                break; // Sai do do-while interno se o salário for válido
+            }
+        } while (1);
+    }
     
-  ENQUANTO opcao != 0
-FIM
+  
+    while (getchar() != '\n'); 
+
+        do {
+                printf("\n======================================\n");
+        printf("  MENU DE OPÇÕES (%d Salários Lidos)\n", N);
+        printf("======================================\n");
+        printf("1) Listar salários\n");
+        printf("2) Média dos salários\n");
+        printf("0) Sair\n");
+        printf("Escolha uma opção: ");
+
+                if (scanf("%d", &opcao) != 1) {
+            printf("\nErro: Opção inválida. Digite apenas um número.\n");
+                       while (
